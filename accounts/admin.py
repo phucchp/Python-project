74b66754225a1288ba051ad9d2a375f6669e6365
phucchp <phucchp@gmail.com>
2023-04-17 10:03:37 +0700

    custom password field

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 # Register your models here.
 
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    list_display=('email','first_name','last_name','username','last_login','is_active')
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
+
+admin.site.register(Account,AccountAdmin)
