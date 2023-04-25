466e804e20a85a4eebde8a44bd528429b2a54ed5
Bảo Long <28072002long@gmail.com>
2023-04-25 23:45:13 +0700

    feat(cart, template, store, db.sqlite3): done product-variation feature and register long super admin

--- carts/views.py
+++ carts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -17,6 +17,20 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
+
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
