9bdaef2ead256463f821dc59e513e1a1179707c8
Bảo Long <28072002long@gmail.com>
2023-04-26 14:21:34 +0700

    feat(store, templates): search function

--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from carts.models import CartItem
 from carts.views import _cart_id
 from .models import Product
 from category.models import Category
+
 
 
 # Create your views here.
@@ -46,3 +48,16 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'store/store.html', context)
